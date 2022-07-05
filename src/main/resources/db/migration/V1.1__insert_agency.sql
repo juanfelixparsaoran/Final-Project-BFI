7ce82abc886bb91308a019860503fb8b545dc51f
Management Trainee Technology Head Office (Juan) <095085@bfi.co.id>
2022-07-05 22:24:17 +0700

    Revert "add sql"
    This reverts commit d2bb45a9f3b1983575b922ed521bcedafde2171a.

--- src/main/resources/db/migration/V1.1__insert_agency.sql
+++ src/main/resources/db/migration/V1.1__insert_agency.sql
@@ -1,2 +1,3 @@
 INSERT INTO `agency`(`code`, `details`, `name`, `owner_user_id`)
-VALUES ("BDL","PT Angkasa Muda","Angkot Karang",1);
+VALUES ("BDL","PT Angkasa Muda","Angkot Karang",1),
+("BDL2","PT Angkasa Tua","Angkot Barang",1);
